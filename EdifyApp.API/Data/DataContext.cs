b01643d05a8abf34aa159002c1872f55bf8b522f
dontonio <dontonio83@gmail.com>
2019-07-23 12:53:53 +0000

    initial Commit

---
+++ EdifyApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using EdifyApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdifyApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+            
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+
+
+}
